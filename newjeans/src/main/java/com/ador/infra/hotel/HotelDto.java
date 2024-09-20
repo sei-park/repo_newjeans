d505346cd3245765353dd3b125116ba4a6a07343
sei-park <tpdlqkr@naver.com>
2024-09-20 17:23:29 +0900

    1

--- newjeans/src/main/java/com/ador/infra/hotel/HotelDto.java
+++ newjeans/src/main/java/com/ador/infra/hotel/HotelDto.java
@@ -16,6 +16,24 @@ public class HotelDto {
 	private Date htRegDate;
 	@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
 	private Date htReviseDate;
+	private String htNotice;
+	private String htMap;
+	private Integer htInclusionItems;
+	private Integer htNameSearch;
+	private Integer htPriceSearch;
+	private Integer htStarSearch;
+	private Integer htClassSearch;
+	private Integer htRestaurantNy;
+	private Integer htSpaNy;
+	private Integer htFitnessCenterNy;
+	private Integer htPoolNy;
+	private Integer htloungeNy;
+	private Integer htStockRoomNy;
+	private Integer htParkingNy;
+	private Integer htShuttleBusNy;
+	private Integer htBreakfastNy;
+	private Integer htRoomServiceNy;
+	private Integer htElseNy;
 	//--
 	public String getHtseq() {
 		return htseq;
@@ -65,6 +83,120 @@ public class HotelDto {
 	public void setHtReviseDate(Date htReviseDate) {
 		this.htReviseDate = htReviseDate;
 	}
+	public String getHtNotice() {
+		return htNotice;
+	}
+	public void setHtNotice(String htNotice) {
+		this.htNotice = htNotice;
+	}
+	public String getHtMap() {
+		return htMap;
+	}
+	public void setHtMap(String htMap) {
+		this.htMap = htMap;
+	}
+	public Integer getHtInclusionItems() {
+		return htInclusionItems;
+	}
+	public void setHtInclusionItems(Integer htInclusionItems) {
+		this.htInclusionItems = htInclusionItems;
+	}
+	public Integer getHtNameSearch() {
+		return htNameSearch;
+	}
+	public void setHtNameSearch(Integer htNameSearch) {
+		this.htNameSearch = htNameSearch;
+	}
+	public Integer getHtPriceSearch() {
+		return htPriceSearch;
+	}
+	public void setHtPriceSearch(Integer htPriceSearch) {
+		this.htPriceSearch = htPriceSearch;
+	}
+	public Integer getHtStarSearch() {
+		return htStarSearch;
+	}
+	public void setHtStarSearch(Integer htStarSearch) {
+		this.htStarSearch = htStarSearch;
+	}
+	public Integer getHtClassSearch() {
+		return htClassSearch;
+	}
+	public void setHtClassSearch(Integer htClassSearch) {
+		this.htClassSearch = htClassSearch;
+	}
+	public Integer getHtRestaurantNy() {
+		return htRestaurantNy;
+	}
+	public void setHtRestaurantNy(Integer htRestaurantNy) {
+		this.htRestaurantNy = htRestaurantNy;
+	}
+	public Integer getHtSpaNy() {
+		return htSpaNy;
+	}
+	public void setHtSpaNy(Integer htSpaNy) {
+		this.htSpaNy = htSpaNy;
+	}
+	public Integer getHtFitnessCenterNy() {
+		return htFitnessCenterNy;
+	}
+	public void setHtFitnessCenterNy(Integer htFitnessCenterNy) {
+		this.htFitnessCenterNy = htFitnessCenterNy;
+	}
+	public Integer getHtPoolNy() {
+		return htPoolNy;
+	}
+	public void setHtPoolNy(Integer htPoolNy) {
+		this.htPoolNy = htPoolNy;
+	}
+	public Integer getHtloungeNy() {
+		return htloungeNy;
+	}
+	public void setHtloungeNy(Integer htloungeNy) {
+		this.htloungeNy = htloungeNy;
+	}
+	public Integer getHtStockRoomNy() {
+		return htStockRoomNy;
+	}
+	public void setHtStockRoomNy(Integer htStockRoomNy) {
+		this.htStockRoomNy = htStockRoomNy;
+	}
+	public Integer getHtParkingNy() {
+		return htParkingNy;
+	}
+	public void setHtParkingNy(Integer htParkingNy) {
+		this.htParkingNy = htParkingNy;
+	}
+	public Integer getHtShuttleBusNy() {
+		return htShuttleBusNy;
+	}
+	public void setHtShuttleBusNy(Integer htShuttleBusNy) {
+		this.htShuttleBusNy = htShuttleBusNy;
+	}
+	public Integer getHtBreakfastNy() {
+		return htBreakfastNy;
+	}
+	public void setHtBreakfastNy(Integer htBreakfastNy) {
+		this.htBreakfastNy = htBreakfastNy;
+	}
+	public Integer getHtRoomServiceNy() {
+		return htRoomServiceNy;
+	}
+	public void setHtRoomServiceNy(Integer htRoomServiceNy) {
+		this.htRoomServiceNy = htRoomServiceNy;
+	}
+	public Integer getHtElseNy() {
+		return htElseNy;
+	}
+	public void setHtElseNy(Integer htElseNy) {
+		this.htElseNy = htElseNy;
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 	
