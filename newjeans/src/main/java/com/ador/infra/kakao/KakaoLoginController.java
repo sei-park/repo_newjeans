088c084ec412ef72534f97d75a58e29fb70525cd
sei-park <tpdlqkr@naver.com>
2025-09-11 21:56:31 +0900

    20250911

--- newjeans/src/main/java/com/ador/infra/kakao/KakaoLoginController.java
+++ newjeans/src/main/java/com/ador/infra/kakao/KakaoLoginController.java
@@ -16,7 +16,7 @@ import okhttp3.Response;
 import org.json.JSONObject;
 
 @Controller
-public class KakaoLoginController {
+public class KakaoLoginController {   
 	
 	// KakaoLoginService 에서 사용자 정보를 가져오고 KakaoLoginController 에서 이를 처리함
 	// 카카오 로그인 요청을 처리하고 콜백을 통해 사용자 정보를 받아 세션에 저장
