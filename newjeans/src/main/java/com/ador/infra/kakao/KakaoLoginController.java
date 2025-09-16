c5924fd65414a41acdb5a8a6c57a08725b96c537
sei-park <tpdlqkr@naver.com>
2025-09-16 23:35:10 +0900

    0916

--- newjeans/src/main/java/com/ador/infra/kakao/KakaoLoginController.java
+++ newjeans/src/main/java/com/ador/infra/kakao/KakaoLoginController.java
@@ -14,7 +14,7 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.json.JSONObject;
-
+  
 @Controller
 public class KakaoLoginController {   
 	
--- newjeans/src/main/java/com/ador/infra/kakao/KakaoLoginService.java
+++ newjeans/src/main/java/com/ador/infra/kakao/KakaoLoginService.java
@@ -5,7 +5,7 @@ import org.json.JSONObject;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-
+   
 @Service
 public class KakaoLoginService {
 	
